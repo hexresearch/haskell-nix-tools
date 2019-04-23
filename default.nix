262b63ce2c282ce82c77a72975f5ca5382e80ce9
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-04-23 11:20:56 +0300

    Initial commit

---
+++ default.nix
@@ -0,0 +1,16 @@
+# common Nix utils for building haskell projects 
+pkgs:
+let
+  gitignore = pkgs.callPackage (pkgs.fetchFromGitHub {
+    owner  = "siers";
+    repo   = "nix-gitignore";
+    rev    = "ce0778ddd8b1f5f92d26480c21706b51b1af9166";
+    sha256 = "1d7ab78i2k13lffskb23x8b5h24x7wkdmpvmria1v3wb9pcpkg2w";
+  }) {};
+
+in
+{
+  # Ignore source from 
+  ignoreSources = ignore-list: source: gitignore.gitignoreSourceAux ignore-list source;
+  hask          = import ./hask.nix pkgs.haskell.lib;
+}
---
+++ hask.nix
@@ -0,0 +1,16 @@
+{ overrideCabal }:
+{
+  # Add build flags to cabal
+  addBuildFlags = flags: drv: overrideCabal drv (drv: {
+    buildFlags = (drv.buildFlags or []) ++ flags;
+  });
+
+  # Build derivation with -Wall flag
+  doWall     = addBuildFlags ["--ghc-option=-Wall"];
+
+  # Build derivation with -Wall and -Werror
+  doPedantic = addBuildFlags ["--ghc-option=-Wall" "--ghc-option=-Werror"];
+
+  # Build without optimizations
+  doNoOptimizations = addBuildFlags ["--ghc-option=-O0"];
+}
